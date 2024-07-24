3ac8c2d4c6ef4da08e84b4ea5a0d953f0ad5bb7b
Takeda-Forever <tbauka2006@gmail.com>
2024-07-24 12:20:59 +0500

    Renamed and add new solves

--- main.cpp
+++ main.cpp
@@ -117,12 +117,96 @@ struct ListNode {
 //    return s;
 //}
 
+// int minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
+//     vector<pair<int, int>> hCuts, vCuts;
+//     for (int i = 0; i < horizontalCut.size(); ++i) {
+//         hCuts.push_back({horizontalCut[i], i});
+//     }
+//     for (int j = 0; j < verticalCut.size(); ++j) {
+//         vCuts.push_back({verticalCut[j], j});
+//     }
+//     sort(hCuts.rbegin(), hCuts.rend());
+//     sort(vCuts.rbegin(), vCuts.rend());
+//     int hCutsCount = 1, vCutsCount = 1;
+//     int i = 0, j = 0;
+//     int totalCost = 0;
+//     while (i < hCuts.size() && j < vCuts.size()) {
+//         if (hCuts[i].first >= vCuts[j].first) {
+//             totalCost += hCuts[i].first * vCutsCount;
+//             ++hCutsCount;
+//             ++i;
+//         } else {
+//             totalCost += vCuts[j].first * hCutsCount;
+//             ++vCutsCount;
+//             ++j;
+//         }
+//     } 
+//     while (i < hCuts.size()) {
+//         totalCost += hCuts[i].first * vCutsCount;
+//         ++i;
+//     }
+//     while (j < vCuts.size()) {
+//         totalCost += vCuts[j].first * hCutsCount;
+//         ++j;
+//     }
+//     return totalCost;
+// }
+
+/*
+
+Example 1:
+
+Input: mapping = [8,9,4,0,2,1,3,5,7,6], nums = [991,338,38]
+Output: [338,38,991]
+Explanation: 
+Map the number 991 as follows:
+1. mapping[9] = 6, so all occurrences of the digit 9 will become 6.
+2. mapping[1] = 9, so all occurrences of the digit 1 will become 9.
+Therefore, the mapped value of 991 is 669.
+338 maps to 007, or 7 after removing the leading zeros.
+38 maps to 07, which is also 7 after removing leading zeros.
+Since 338 and 38 share the same mapped value, they should remain in the same relative order, so 338 comes before 38.
+Thus, the sorted array is [338,38,991].
 
 
+mappping[nums[i][i]] = 6
+9 = 6
+1 = 9
+
+*/
+
+vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+    unordered_map<int, int> mappingMap;
+    for(auto by : mapping) 
+        mappingMap[by] = by;
+
+    list<int> numbers;
+    for(int i  = nums[0]; i > 10; i / 10)
+        numbers.push_front(i % 10);
+    
+    return mapping;
+}
 
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);
 
+    arr_i _mapping{8,9,4,0,2,1,3,5,7,6}, 
+          _nums{991,338,38};
+
+    arr_i nums = sortJumbled(_mapping, _nums);
+    
+    // for(auto& by : nums)
+    //     cout << by;
+
+
+    list<int> numbers;
+    for(int i  = _nums[0]; i > 10; i / 10)
+        numbers.push_front(i % 10);
+
+    for(auto nm : numbers)
+        cout <<  "Value-of-element -> "<< nm << endl;
+    
+    
     return 0;
 }
